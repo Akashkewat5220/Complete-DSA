ad2f2157d66b05af8e4e66c1c86c4b8b83ee3ccb
Akashkewat5220 <102970062+Akashkewat5220@users.noreply.github.com>
2024-01-12 21:01:27 +0530

    Add files via upload

---
+++ Day_23 (Array Problems)/Fibbonacci_series.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    int arr[1000];
+
+    // first = 0 ;
+    // secont = 1;
+
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for(int i = 2; i<=n-1 ; i++)
+    arr[i] = arr[i-1] + arr[i - 2];
+
+    cout<<arr[n-1]<<" ";
+}
