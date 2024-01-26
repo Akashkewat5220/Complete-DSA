1b34df45b62b98c1dafebcfcdf60ead999e2d043
Akashkewat5220 <102970062+Akashkewat5220@users.noreply.github.com>
2024-01-26 23:34:24 +0530

    Add files via upload

---
+++ Day_64 (Pointer with Array)/ptr.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int arr[5] = {1,2,3,4,5};
+    // int *ptr = arr;
+
+    //address of the first element
+    // cout<<arr<<endl;
+    // cout<<arr+0<<endl;
+    // cout<<&arr[0]<<endl;
+    // cout<<ptr<<endl;
+
+    // //address of the second element
+    // cout<<arr+1<<endl;
+    // cout<<&arr[1]<<endl;
+
+    // //print the value
+    // cout<<arr[0]<<endl;
+    // cout<<*arr<<endl;
+    // cout<<*(arr+0)<<endl;
+    // cout<<*ptr<<endl;
+
+
+    // //print alll address
+    // for(int i = 0 ; i < 5 ; i++)
+    // cout<<arr+i<<endl;
+
+    // // print all values of the address
+    // for(int i = 0 ; i< 5 ; i++)
+    // cout<<*(arr + i )<<endl;
+
+    // all vlaues by another way
+    // for(int i = 0 ; i < 5 ; i++)
+    // cout<<ptr[i]<<" ";
+
+    // // print all the address
+    // for(int i = 0 ; i< 5 ; i++)
+    // cout<<ptr + i <<endl;
+
+    // //arithmetic operations
+    // for(int i = 0 ; i < 5 ; i++)
+    // {
+    //     cout<<*ptr<<" ";
+    //     ptr++;
+        
+    // }
+
+    // // negative operations
+    // for(int i = 5 ; i >= 0 ; i--)
+    // {
+    //     cout<<*ptr<<" ";
+    //     ptr--;
+    // }
+
+    
+
+
+//homework
+// char name = 'a';
+// char *ptr = &name;
+// cout<<ptr;
+
+
+
+
+} 
