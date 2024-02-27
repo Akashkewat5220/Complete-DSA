e82c461e186b23b5fd9ee4e0a08331bfd8006acc
Akashkewat5220 <102970062+Akashkewat5220@users.noreply.github.com>
2024-02-27 19:34:28 +0530

    Add files via upload

---
+++ Day_135 (Stack Problems Day)/problemsName.cpp
@@ -0,0 +1,9 @@
+// In this day I have solved Problems on Stack.
+
+// PROBLEMS NAME.
+
+//  1. Next Greater Element
+//  2. Next Smaller Element
+//  3. Stock Span Problem
+//  4. Next Greater Element 2
+//  5. Smallest Number on Left
---
+++ Day_136 (Hard Stack Questions)/problems.cpp
@@ -0,0 +1,4 @@
+// I have done 3 Problems today based on stack.
+
+// 1. Largest Rectangle in Histogram
+// 2. Maximal Rectangle
