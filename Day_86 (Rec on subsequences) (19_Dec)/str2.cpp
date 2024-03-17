52d6c447c9c54d151900c77605a2d7ae9a758a34
Akashkewat5220 <102970062+Akashkewat5220@users.noreply.github.com>
2024-03-17 20:56:41 +0530

    Add files via upload

---
+++ Day_86 (Rec on subsequences) (19_Dec)/str2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+void subset(string  &s , int index , int n , vector<string>&ans , string &temp)
+{
+    if(index == n)
+    {
+        ans.push_back(temp);
+        return;
+    }
+
+
+    //not include
+    subset(s , index +1  ,n, ans , temp);
+
+    //include
+    temp.push_back(s[index]);
+    subset(s , index+1 , n  , ans , temp);
+    temp.pop_back();
+}
+int main()
+{
+     string s = "abcd";
+    vector<string>ans;
+    string temp;
+    subset(s , 0 , s.size() , ans , temp);
+    for(int i = 0 ; i <ans.size() ; i++)
+    cout<<ans[i]<<endl;
+}
