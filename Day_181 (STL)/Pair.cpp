1d099f45705d6cd82697a432421d2ab621355ad4
Akashkewat5220 <102970062+Akashkewat5220@users.noreply.github.com>
2024-03-25 12:37:37 +0530

    Add files via upload

---
+++ Day_181 (STL)/Pair.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // pair<string , int>p;
+
+
+    // p = make_pair("rohit" , 90);
+
+    // p.first = "alas";
+    // p.second = 90;
+
+    // three value in the pairs
+    pair<string , pair<int , int> >p;
+    // p.first = "alas";
+    // p.second.first = 23 ;
+    // p.second.second = 9;
+
+    cout<<p.first<<" "<<p.second.first<<" "<<p.second.second;
+}
